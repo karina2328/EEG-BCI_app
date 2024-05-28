8acf4d4af79f0a4ba189f8382613dd97b2cb7a21
karina2328 <133897781+karina2328@users.noreply.github.com>
2024-05-28 22:42:21 +0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -11,12 +11,12 @@ class CmdPopup(Popup):
         layout = MDBoxLayout(orientation='vertical', spacing=10, padding=15)
         cmd_layout = MDGridLayout(cols=2, spacing=50, padding=25)
         self.title = 'Выбор команд'
-        cmd_button1 = MDButton(MDButtonText(text='Разжать',pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c1)
-        cmd_button2 = MDButton(MDButtonText(text='Сжать',pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c2)
-        cmd_button3 = MDButton(MDButtonText(text='Жест 1',pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c3)
-        cmd_button4 = MDButton(MDButtonText(text='Жест 2',pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c4)
-        cmd_button5 = MDButton(MDButtonText(text='Жест 3',pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c5)
-        cmd_button6 = MDButton(MDButtonText(text='Жест 4',pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c6)
+        cmd_button1 = MDButton(MDButtonText(text='Разжать', pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c1)
+        cmd_button2 = MDButton(MDButtonText(text='Сжать', pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c2)
+        cmd_button3 = MDButton(MDButtonText(text='Жест 1', pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c3)
+        cmd_button4 = MDButton(MDButtonText(text='Жест 2', pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c4)
+        cmd_button5 = MDButton(MDButtonText(text='Жест 3', pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c5)
+        cmd_button6 = MDButton(MDButtonText(text='Жест 4', pos_hint={'center_x': 0.5, 'center_y':0.5}), on_release=self.c6)
         cmd_button7 = MDButton(MDButtonText(text='Жест 5', pos_hint={'center_x': 0.5, 'center_y': 0.5}),
                                on_release=self.c3)
         cmd_button8 = MDButton(MDButtonText(text='Жест 6', pos_hint={'center_x': 0.5, 'center_y': 0.5}),
@@ -50,19 +50,19 @@ class CmdPopup(Popup):
 
         self.uuid_hand = '6E400002-B5A3-F393-E0A9-E50E24DCCA9E'
     def c1(self,btn):
-        self.cmd_for_hand = '80805050ff'
+        self.cmd_for_hand = '80805050ff81815050ff82825050ff83835050ff84845050ff'
         self.send_cmd(self.cmd_for_hand)
     def c2(self,btn):
-        self.cmd_for_hand = 'c0c05050ff'
+        self.cmd_for_hand = 'c0c05050ffc1c15050ffc2c25050ffc3c35050ffc4c45050ff'
         self.send_cmd(self.cmd_for_hand)
     def c3(self,btn):
-        self.cmd_for_hand = '81815050ff'
+        self.cmd_for_hand = '81815050ffc2c25050ff82826060ff'
         self.send_cmd(self.cmd_for_hand)
     def c4(self,btn):
-        self.cmd_for_hand = 'c1c15050ff'
+        self.cmd_for_hand = 'c1c12020ff'
         self.send_cmd(self.cmd_for_hand)
     def c5(self,btn):
-        self.cmd_for_hand = '82825050ff'
+        self.cmd_for_hand = '82827070ffc0c01010ff'
         self.send_cmd(self.cmd_for_hand)
     def c6(self,btn):
         self.cmd_for_hand = 'c2c25050ff'
